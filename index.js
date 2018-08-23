ba17be16644965c492809a7378498aab1840ace0
GabrielAvilesR <gabrielaviles1995@gmail.com>
2018-08-22 21:25:19 -0500

    hello world using bootstrap with react

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import path from 'path';
 import express from 'express';
 import bodyParser from 'body-parser';
 
+import apiView from './src/Router'
+
 const server = express();
 server.use(bodyParser.json());
 
@@ -14,6 +16,7 @@ server.use(sassMiddleware({
 
 server.set('view engine', 'ejs');
 server.use(express.static('public'));
+server.use(apiView)
 
 server.listen(config.port, config.host, () => {
   console.info('Express listening on port', config.port);
